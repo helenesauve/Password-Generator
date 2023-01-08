a1a097d8d01d253ed79572bbf5625768adbf29d7
Hélène Sauvé <helenesauve@Helenes-MacBook-Pro.local>
2023-01-08 10:09:12 +0000

    creating the template for the README

--- starter/script.js
+++ starter/script.js
@@ -1,136 +1,177 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+// creating array with all arrays
+
+var passwordoptions = [
+  specialCharacters,
+  numericCharacters,
+  lowerCasedCharacters,
+  upperCasedCharacters,
 ];
 
 // Function to prompt user for password options
 // Ask how many characters do you want? Between 10 and 64
 // Ask what character types do you want? Lowercase, Uppercase, Numeric, Special characers
+
 function getPasswordOptions() {
-  var charNum = prompt("What length do you want for your password (min 10, max 64?");
+  var charNum = prompt(
+    "What length do you want for your password (min 10, max 64?"
+  );
   if (charNum >= 10 && charNum <= 64) {
-  return charNum
+    console.log(charNum);
+  } else {
+    prompt("Character needs to be a number between 10 and 64");
   }
-  else {
-  (prompt("Character needs to be a number between 10 and 64"))
+
+  // prompt password options
+  do {
+    var includeLowercase = confirm("Click Ok for lowercase");
+    if (includeLowercase === true) {
+      console.log(includeLowercase);
+    }
+    var includeUpperCase = confirm("Click Ok for uppercase");
+    if (includeUpperCase === true) {
+      console.log(includeUpperCase);
+    }
+    var includeNumber = confirm("Click Ok for numbers");
+    if (includeNumber === true) {
+      console.log(includeNumber);
+    }
+    var includeSpecialChar = confirm("Click Ok for special characters");
+    if (includeSpecialChar === true) {
+      console.log(includeSpecialChar);
+    }
+    //if none selected, prompt that at least one needs to be selected, otherwise run code again
+  } while (
+    includeLowercase === false &&
+    includeUpperCase === false &&
+    includeNumber === false &&
+    includeSpecialChar === false
+  );
+// return values in an object
+  return {
+    length: charNum,
+    lowerCase: includeLowercase,
+    upperCase: includeUpperCase,
+    numeric: includeNumber,
+    specialchar: includeSpecialChar,
+  };
 }
-}
 
-getPasswordOptions();
 
-var lowercase = confirm("Do you want lowercase letters?\nEither OK or No.");
-var upperCase = confirm("Do you want uppercase letters?\nEither OK or No.");
-var numer = confirm("Do you want numbers?\nEither OK or No.");
-var specialChar = confirm("Do you want special characters?\nEither OK or No.");
-
-//if none selected, prompt that at least one needs to be selected, otherwise run code again
 
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  // filter and create new array of chosen array
+  // concatenate chosen arrays
+  // use for loop and math random to pick items based on chosen number of characters
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+  getPasswordOptions();
+  return "This is a secret password. Whee.";
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
